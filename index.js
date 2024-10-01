83655f926144a3ca7072c0f94fbefbf528b81e64
geraldine-l <geraldine.lonchay@gmail.com>
2024-10-01 11:54:50 +0200

    add color yellow

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
 console.log(chalk.blue('Hello Thomas!'));
-console.log(chalk.orange('Hello Julie!'));
+console.log(chalk.yellow('Hello Julie!'));
 console.log(chalk.pink('Hello Agnès!'));
 console.log(chalk.green('Hello Salvia!'));
