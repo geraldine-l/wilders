69d91f8c07ac19a6522676ddf4aa31f6c9026935
geraldine-l <geraldine.lonchay@gmail.com>
2024-10-01 12:02:02 +0200

    change color pink

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
 console.log(chalk.blue('Hello Thomas!'));
 console.log(chalk.yellow('Hello Julie!'));
-console.log(chalk.pink('Hello Agnès!'));
+console.log(chalk.grey('Hello Agnès!'));
 console.log(chalk.green('Hello Salvia!'));
